f6c80732d4e2f69f8f54526c6aca1dd3a52ccb0c
Emmanuel Eze <45445963+realemmanueleze@users.noreply.github.com>
2023-07-27 02:39:28 +0100

    feat(core): created user model

--- server/alter/settings.py
+++ server/alter/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,4 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTH_USER_MODEL = 'core.User'
